e9d8ea763202856889bc67de320bb2654e6078b3
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-11 18:49:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction (name, language)
+def introduction_with_language(name, language)
   "Hi, my name is #{name} andI am leraning to program in #{language}."
 end
